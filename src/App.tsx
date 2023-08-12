159cb55eefc4700da572f3806e74f8e2e5e407c8
David Gridley <davidgridley21@gmail.com>
2023-08-12 13:41:21 +0100

    add input and store value in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
+
 function App() {
+  const [listItem, setListItem] = useState<string>('')
+
+  const handleChange = (value: string) => {
+    setListItem(value)
+  }
+
   return (
     <>
       <h1>To do:</h1>
+      <div>
+        <input
+          value={listItem}
+          onChange={(event) =>
+            handleChange(event.target.value)
+          }
+        />
+      </div>
     </>
   )
 }
