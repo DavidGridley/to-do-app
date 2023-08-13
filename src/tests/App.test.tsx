a3a68f0d08e606c396516fe7dd3ec90221f9b7c4
David Gridley <davidgridley21@gmail.com>
2023-08-13 20:27:02 +0100

    add test for adding multiple items

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,20 +2,41 @@ import { fireEvent, render, screen } from '@testing-library/react';
 
 import App from '../App.tsx';
 
-describe('App', () => {
+beforeEach(() => {
+  window.localStorage.clear();
+  render(<App />);
+});
 
-  it('adds an item to the to do list', () => {
-    render(<App />);
+const addItem = (input: HTMLElement, value: string) => {
+  fireEvent.change(input, { target: { value } });
+}
 
-    const toDoInput = screen.getByTestId('add-item-input');
-    const addButton = screen.getByTestId('add-item-button');
+test('adds an item to the to do list', () => {
+  const toDoInput = screen.getByTestId('add-item-input');
+  const addButton = screen.getByTestId('add-item-button');
 
-    fireEvent.change(toDoInput, { target: { value: "Make lunch" } });
-    fireEvent.click(addButton);
+  addItem(toDoInput, 'Make lunch');
+  fireEvent.click(addButton);
 
-    const toDoList = screen.getAllByRole('listitem');
+  const toDoList = screen.getAllByRole('listitem');
 
-    expect(toDoList).toHaveLength(1)
-  });
+  expect(toDoList).toHaveLength(1);
+});
 
+test('adds multiple item to the to do list', () => {
+  const toDoInput = screen.getByTestId('add-item-input');
+  const addButton = screen.getByTestId('add-item-button');
+
+  addItem(toDoInput, 'Make lunch');
+  fireEvent.click(addButton);
+
+  addItem(toDoInput, 'Go for a walk');
+  fireEvent.click(addButton);
+
+  addItem(toDoInput, 'Finish project');
+  fireEvent.click(addButton);
+
+  const toDoList = screen.getAllByRole('listitem');
+
+  expect(toDoList).toHaveLength(3);
 });
